3472de8b540d79d5fcbce8e6377c6cbc1018049e
Jason Antic <jsa169@gmail.com>
2018-09-17 14:18:38 -0700

    Refactoring visualization code into core fasterai library

--- fasterai/visualize.py
+++ fasterai/visualize.py
@@ -2,6 +2,14 @@ from numpy import ndarray
 from matplotlib.axes import Axes
 from fastai.conv_learner import *
 from fastai.dataset import *
+from IPython.display import display
+
+
+def visualize_image_gen_model(md: ImageData, model: nn.Module, start_idx: int, count: int, figsize=(20,20), max_columns=4, immediate_display=True):
+    end_index = start_idx + count
+    idxs = list(range(start_idx,end_index))
+    plot_image_outputs_from_model(ds=md.val_ds, model=model, idxs=idxs, max_columns=max_columns, immediate_display=immediate_display)
+
 
 def plot_image_from_ndarray(image: ndarray, axes:Axes=None, figsize=(20,20)):
     if axes is None: 
@@ -11,29 +19,31 @@ def plot_image_from_ndarray(image: ndarray, axes:Axes=None, figsize=(20,20)):
     axes.axis('off')
 
 
-def plot_images_from_ndarray_pairs(image_pairs: [(ndarray, ndarray)], figsize=(20,20), max_columns=6):
+def plot_images_from_ndarray_pairs(image_pairs: [(ndarray, ndarray)], figsize=(20,20), max_columns=6, immediate_display=True):
     num_pairs = len(image_pairs)
     num_images = num_pairs * 2
-    rows, columns = get_num_rows_columns(num_images, max_columns)
+    rows, columns = _get_num_rows_columns(num_images, max_columns)
 
-    _, axes = plt.subplots(rows, columns, figsize=figsize)
+    fig, axes = plt.subplots(rows, columns, figsize=figsize)
     for i,(x,y) in enumerate(image_pairs):
         plot_image_from_ndarray(x, axes=axes.flat[i*2])
         plot_image_from_ndarray(y, axes=axes.flat[i*2+1])
 
+    if immediate_display:
+        display(fig)
 
 
 def plot_images_from_dataset(ds: FilesDataset, start_idx: int, count: int, figsize=(20,20), max_columns=6):
     idxs = list(range(start_idx, start_idx+count))
     num_images = len(idxs)
-    rows, columns = get_num_rows_columns(num_images, max_columns)
+    rows, columns = _get_num_rows_columns(num_images, max_columns)
     _,axes=plt.subplots(rows,columns,figsize=figsize)
 
     for idx,ax in zip(idxs, axes.flat):  
         plot_image_from_ndarray(ds.denorm(ds[idx][0])[0], axes=ax)
 
 
-def plot_image_outputs_from_model(ds: FilesDataset, model, idxs: [int], figsize=(20,20), max_columns=6):
+def plot_image_outputs_from_model(ds: FilesDataset, model: nn.Module, idxs: [int], figsize=(20,20), max_columns=6, immediate_display=True):
     image_pairs = []
 
     for idx in idxs:
@@ -41,9 +51,9 @@ def plot_image_outputs_from_model(ds: FilesDataset, model, idxs: [int], figsize=
         preds = model(VV(x[None]))
         image_pairs.append((ds.denorm(x[None])[0], ds.denorm(preds)[0]))
 
-    plot_images_from_ndarray_pairs(image_pairs, figsize=figsize, max_columns=max_columns)
+    plot_images_from_ndarray_pairs(image_pairs, figsize=figsize, max_columns=max_columns, immediate_display=immediate_display)
 
-def get_num_rows_columns(num_images: int, max_columns: int):
+def _get_num_rows_columns(num_images: int, max_columns: int):
     columns = min(num_images, max_columns)
     rows = num_images//columns
     rows = rows if rows * columns == num_images else rows + 1
