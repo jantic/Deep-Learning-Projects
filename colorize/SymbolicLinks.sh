1caf7410f61bfd55f6a1cf24a6bd87e26a04264a
Jason Antic <jsa169@gmail.com>
2018-10-31 16:23:56 -0700

    Moving these to their own separate repo- DeOldify

--- colorize/SymbolicLinks.sh
+++
@@ -1,3 +0,0 @@
-sudo ln -sf "/media/jason/Projects/Deep Learning/fastai/fastai" "./fastai"
-sudo ln -sf "/media/jason/Projects/Deep Learning/Deep Learning Projects/fasterai" "./fasterai"
-sudo ln -sf "/media/jason/Projects/Deep Learning/data" "./data"
